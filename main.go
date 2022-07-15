b2d9f94ae93d003b3e0cf906a56200305d7af8f7
MrNice <supakornhomnan@gmail.com>
2022-07-15 23:24:43 +0700

    decode int list to string and reverse string to real answer

--- main.go
+++ main.go
@@ -11,7 +11,19 @@ func main() {
 
 	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
 
-	sd, _ := base64.StdEncoding.DecodeString(secret)
+	sd, err := base64.StdEncoding.DecodeString(secret)
+
+	if err != nil {
+		fmt.Println("decode error: ", err)
+	}
+
+	decode := string(sd)
+	reverse := func(str string) (result string) {
+		for _, v := range str {
+			result = string(v) + result
+		}
+		return
+	}
+	whatIsIt = reverse(decode)
 	fmt.Println(whatIsIt)
-	fmt.Println(sd)
 }
