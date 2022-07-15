ba46c46a29806f0db9e2be85e01495de2930af89
MrNice <supakornhomnan@gmail.com>
2022-07-15 23:01:21 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mysterious code by LINEMAN Wongnai
+
+[Reference question \<here\>]('https://github.com/LINEMANWongnai/LINE-MAN-Wongnai-Mysterious-Code?fbclid=IwAR2gwXRbq2LQUHn7qKW3EmuTh2pRlMeSPjhSvy7E4PrhBxIRnrh0cvq1RAI')
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+
+	var whatIsIt string
+
+	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
+
+	sd, _ := base64.StdEncoding.DecodeString(secret)
+	fmt.Println(whatIsIt)
+	fmt.Println(sd)
+}
